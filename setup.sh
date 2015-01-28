dbe09c1159bb0723a2746e4e766eefa79c8ff3e6
Sakis Vtdk <sarig1988@gmail.com>
2015-01-28 22:49:34 +0000

    Setup git-completion and git-prompt

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ rm -rf ~/.vim
 rm ~/.bashrc
 rm ~/.vimrc
 rm ~/.bash_aliases
+rm ~/.git-completion.bash
+rm ~/.git-prompt.sh
 
 echo "$prefix Creating ~/.vim/bundle & ~/.vim/colors"
 mkdir -p ~/.vim/bundle/
@@ -14,6 +16,10 @@ curl -Gk https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.
 
 echo "$prefix Downloading Vundle"
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+echo "$prefix Downloading latest git-completion && git-prompt"
+curl -Gk https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
+curl -Gk https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
 
 echo "$prefix Creating soft links for ~/.bashrc, ~/.vimrc, ~/.bash_aliases"
 path=`pwd`
